420aec1ea723fd7b35b5536c43cd9f549170e04f
klimov-rv <klimov.rv@yandex.ru>
2021-04-24 12:15:41 +0300

    fix iteration1

--- index_files/index.js
+++ index_files/index.js
@@ -37,3 +37,19 @@ document.addEventListener("DOMContentLoaded", () => {
   window.addEventListener("load", scroll);
   window.addEventListener("scroll", scroll);
 });
+
+// фильтр кандидатов по городу
+var filter_select_el = document.getElementById('selectcity');
+var items_el = document.getElementById('candidates');
+
+filter_select_el.onchange = function() {
+	console.log(this.value);
+  var items = items_el.getElementsByClassName('candidates__card');
+  for (var i=0; i<items.length; i++) {
+  	if (items[i].classList.contains(this.value)) {
+    	items[i].style.display = 'flex';
+    } else {
+    	items[i].style.display = 'none';
+    }
+  }
+};
